ff9512dd96856b872ed55db0d75c3860557b509f
Mohsin Jesani <mohsin.jesani@hotmail.com>
2021-03-18 20:18:41 +0530

    Added repository and AutoMapper

---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface IUserRepository
+    {
+        void update(AppUser user);
+        Task<bool> SaveAllAsync();
+        Task<AppUser> GetUserByIdAsync(int id);
+        Task<AppUser> GetUserByUsernameAsync(string username);
+        Task<IEnumerable<AppUser>> GetUsersAsync();
+        Task<IEnumerable<MembersDto>> GetMembers();
+        Task<MembersDto> GetMember(string username);
+    }
+}
